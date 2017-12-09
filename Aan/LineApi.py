f2b811002b7463bf8031903ce437e35c68fe395d
fiks12 <34192311+fiks12@users.noreply.github.com>
2017-12-09 17:30:54 +0700

    Update LineApi.py

--- Aan/LineApi.py
+++ Aan/LineApi.py
@@ -27,7 +27,7 @@ class LINE:
   def __init__(self):
     self.Talk = Talk()
     self._session = requests.session()
-    self._headers = {'X-Line-Application': 'DESKTOPMAC 10.10.2-YOSEMITE-x64 MAC 4.5.0', 'X-Line-Access': 'En9F8NgFpnCwt5FRGu3d.UvZQ8iaX1kDhGbAShThZ3q.BPHNVeQNnfow7SZ1SGynNg7eghAGXSqFPNpcJBcd3LI=', 'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'}
+    self._headers = {'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0', 'X-Line-Access': 'En9F8NgFpnCwt5FRGu3d.UvZQ8iaX1kDhGbAShThZ3q.BPHNVeQNnfow7SZ1SGynNg7eghAGXSqFPNpcJBcd3LI=', 'User-Agent': 'Line/7.14.0'}
 
   def login(self, mail=None, passwd=None, cert=None, token=None, qr=False, callback=None):
     if callback is None:
