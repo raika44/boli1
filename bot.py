193d14e9ce9a93b59021900ed88a162e913ec41b
fiks12 <34192311+fiks12@users.noreply.github.com>
2017-12-10 17:19:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -672,14 +672,24 @@ def bot(op):
                 cl.sendText(msg.to,'Error.')
 #----------------------------------------------------------------------------
 #---------------------------- SPAM CHAT -------------------------------------
-            elif "/spam " in msg.text:
-                if msg.from_ in admin:
-                    txt = msg.text.split(" ")
-                    jmlh = int(txt[1])
-                    teks = msg.text.replace("/spam " + str(jmlh) + " ","")
-                    if jmlh <= 999:
-                        for x in range(jmlh):
-                            cl.sendText(msg.to,teks)
+            elif "Spam " in msg.text:
+              if msg.from_ in admin and owner:
+                   txt = msg.text.split(" ")
+                   jmlh = int(txt[2])
+                   teks = msg.text.replace("Spam "+str(txt[1])+" "+str(jmlh)+ " ","")
+                   tulisan = jmlh * (teks+"\n")
+                   #Keke cantik <3
+                   if txt[1] == "on":
+                        if jmlh <= 10000:
+                             for x in range(jmlh):
+                                   cl.sendText(msg.to, teks)
+                        else:
+                               cl.sendText(msg.to, "Out of range! ")
+                   elif txt[1] == "off":
+                         if jmlh <= 10000:
+                               cl.sendText(msg.to, tulisan)
+                         else:
+                               cl.sendText(msg.to, "Out of range! ")
 #----------------------------------------------------------------------------
 #---------------------------- CHANGE GROUP NAME -----------------------------
             elif "/gn " in msg.text:
