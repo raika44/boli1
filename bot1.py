351149cc4bc27ed450ae8c419021a5ce426c9c8b
fiks12 <34192311+fiks12@users.noreply.github.com>
2017-12-22 04:51:09 +0700

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -17,7 +17,7 @@ import html5lib
 
 cl = Aan.LINE()
 #cl.login(qr=True)
-cl.login(token="EoSlrq9d3zpMpkzFCxTb.VHH0q0Dhr8pSns5/+RsmgW.zx7C2I7cFTYeV30zT8LBfTuMe9TgpkBPxl9JKYZDt4o=")
+cl.login(token="EoQWRbeN7FPR5gzhOyvb.VHH0q0Dhr8pSns5/+RsmgW.CPUNaL2uPZmGbAAiahXHnSxhdOxPEABgVlFFTtNKPvw=")
 cl.loginResult()
 
 print "login success"
